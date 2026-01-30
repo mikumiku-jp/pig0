f3d065549c4345d784ae9cde836a81c824bbf033
mikumiku-jp <mikumiku-jp@users.noreply.github.com>
2026-01-30 18:11:23 +0900

    バグ修正
    関係ないダメージ与えても満腹度全回復する問題を修正

--- src/main/java/jp/mikumiku/pig0/event/PlayerAttackEventHandler.java
+++ src/main/java/jp/mikumiku/pig0/event/PlayerAttackEventHandler.java
@@ -1,8 +1,6 @@
 package jp.mikumiku.pig0.event;
 
 import jp.mikumiku.pig0.Pig0Mod;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.food.FoodData;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,12 +10,5 @@ public class PlayerAttackEventHandler {
 
     @SubscribeEvent
     public static void onLivingDamage(LivingDamageEvent event) {
-        if (event.getSource().getEntity() instanceof Player player) {
-            if (!player.level().isClientSide()) {
-                FoodData foodData = player.getFoodData();
-                foodData.setFoodLevel(20);
-                foodData.setSaturation(20.0F);
-            }
-        }
     }
 }
